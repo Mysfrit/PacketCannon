0920731fbfce73c599cd1e0f5bbcb8b776fcdf37
Mystify <misfryt@gmail.com>
2020-05-30 16:22:49 +0200

    More fixes

--- WpfApp2/Properties/Annotations.cs
+++ WpfApp2/Properties/Annotations.cs
@@ -32,7 +32,7 @@ using System;
 // ReSharper disable MemberCanBeProtected.Global
 // ReSharper disable InconsistentNaming
 
-namespace WpfApp2.Annotations
+namespace PacketCannon.Properties
 {
     /// <summary>
     /// Indicates that the value of the marked element could be <c>null</c> sometimes,
